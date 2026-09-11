afa14fdd16701014b39e1afd2e7b8492b4cc7bb6
DefNotArham <arhamkabir231@gmail.com>
2026-09-11 10:37:42 -0400

    Create practice.sql

---
+++ practice.sql
@@ -0,0 +1,41 @@
+CREATE TABLE students (
+    id INTEGER,
+    name TEXT,
+    age INTEGER
+);
+
+INSERT INTO students (id, name, age)
+VALUES
+    (123, 'ARHAM', 17),
+    (1234, 'SQL', 17);
+
+SELECT id
+FROM students
+WHERE id = 123;
+
+SELECT name
+FROM students
+WHERE name = 'ARHAM';
+
+SELECT id, name
+FROM students
+WHERE id = 1234 AND name = 'SQL';
+
+SELECT id, name
+FROM students
+WHERE id = 1234 OR age = 17;
+
+SELECT age
+FROM students
+WHERE age = 16 OR age = 17;
+
+UPDATE students
+SET age = 18
+WHERE id = 1234;
+
+DELETE from students
+WHERE id = 1234;
+
+SELECT age
+FROM students
+ORDER BY age ASC
