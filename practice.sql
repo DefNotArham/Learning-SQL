c4153250d150322c37d8a813bd128578ed754934
DefNotArham <arhamkabir231@gmail.com>
2026-09-11 10:39:03 -0400

    Select or and and operator

--- practice.sql
+++ practice.sql
@@ -12,3 +12,19 @@ VALUES
 SELECT id
 FROM students
 WHERE id = 123;
+
+SELECT name
+FROM students
+WHERE name = 'ARHAM';
+
+SELECT id, name
+FROM students
+WHERE id = 1234 AND name = 'SQL';
+
+SELECT id, name
+FROM students
+WHERE id = 1234 OR age = 17;
+
+SELECT age
+FROM students
+WHERE age = 16 OR age = 17;
